ca2d35aa2d37c2e9069a50f4cf7f827e0696009f
mightbegood12 <banunaresh1976@gmail.com>
2024-09-22 15:20:07 +0530

    [ReFactor] Carousel

--- src/components/ImageSlicer.jsx
+++ src/components/ImageSlicer.jsx
@@ -1,5 +1,24 @@
 const ImageSlicer = () => {
-  return <section></section>;
+  return (
+    <section aria-label="Grocery-Carousel">
+      <ul>
+        <li className="slide">
+          <img
+            src="https://i.pinimg.com/originals/6e/76/ab/6e76ab43f1cb5de5f89513114b9a660b.jpg"
+            alt="Samurai"
+          />
+          <img
+            src="https://unsplash.com/photos/a-picture-of-a-black-and-orange-object-Y7jY9iblRKM"
+            alt="Samurai"
+          />
+          <img
+            src="https://i.pinimg.com/originals/6e/76/ab/6e76ab43f1cb5de5f89513114b9a660b.jpg"
+            alt="Samurai"
+          />
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default ImageSlicer;

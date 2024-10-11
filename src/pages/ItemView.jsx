aeaf955e3b09df3852d87abd25f561adac802929
mightbegood12 <banunaresh1976@gmail.com>
2024-10-11 20:48:20 +0530

    [Fix] ItemView UI

--- src/pages/ItemView.jsx
+++ src/pages/ItemView.jsx
@@ -16,38 +16,51 @@ export default function ItemView() {
   console.log(productId);
   return (
     <>
-    <div className=" flex h-auto justify-center min-h-screen">
-      <div className="wrapper flex m-5 p-2 flex-row gap-72">
-        <div className="item-img-cont  h-[470px] w-[464px] p-8 flex flex-col gap-5 justify-center items-center">
-        <div className="big-img">
-            <img src={select} className="h-80" alt="" />
+      <div className=" flex h-auto  justify-center ">
+        <div className="wrapper flex m-2 p-2 flex-row justify-between gap-8">
+          <div className="item-img-cont flex-grow w-[50%] p-8 flex flex-col gap-5 justify-center items-center">
+            <div className="big-img">
+              <img
+                src={select}
+                className="h-[24rem] w-[24rem] object-cover object-center transition-opacity duration-300 ease-in-out"
+                alt=""
+              />
+            </div>
+            <div className="sml-imgs flex flex-row gap-5 items-start justify-start">
+              {smallImages.map((img, i) => (
+                <div
+                  key={i}
+                  className={`border p-3 rounded-lg  hover:scale-110 hover:bg-gray-300 hover:bg-opacity-20 duration-200 ${
+                    select === img ? "border-gray-400" : "border-transparent"
+                  }`}
+                  onClick={() => setSelect(img)}
+                >
+                  <img
+                    className="object-cover object-center rounded-lg hover:cursor-pointer w-16 h-12"
+                    src={img}
+                    alt=""
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="">
+              <ProductDetails />
+            </div>
           </div>
-          <div className="sml-imgs flex flex-row gap-5 items-start justify-start">
-            {smallImages.map((img, i) => (
-              <div
-                key={i}
-                className={`border p-3 rounded-lg transition-all duration-200 ${
-                  select === img ? "border-gray-700" : "border-transparent"
-                }`}
-                onClick={() => setSelect(img)}
-              >
-                <img className="hover:cursor-pointer w-16 h-12" src={img} alt="" />
-              </div>
-            ))}
+          <div className="h-full w-[1px] bg-gray-300 bg-opacity-40"></div>
+          <div className="item-info sticky w-[50%] p-8">
+            <div className="category text-gray-500 text-sm">Milk</div>
+            <div className="title text-2xl font-semibold">Icecream</div>
+            <div className="price text-lg">Price: $399</div>
+            <button
+              className="h-10 p-2  bg-red-600 text-white rounded-lg cursor-pointer"
+              // onClick={}
+            >
+              Add to cart
+            </button>
           </div>
-        </div>
-        <div className="item-info bg-slate-100 w-[464px]  p-8">
-          <div className="category text-gray-500 text-sm">Milk</div>
-          <div className="title text-xl font-semibold">Icecream</div>
-          <div className="price">399</div>
         </div>
       </div>
-    </div>
-    <div className=" wrapper flex  p-2 flex-row gap-4">
-      <div className=" item-img-cont  h-[464px] w-[464px] p-8 flex flex-col ml-14 gap-5 justify-center items-center">
-          <ProductDetails />
-        </div>
-        </div>
     </>
   );
 }

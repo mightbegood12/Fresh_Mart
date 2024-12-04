996d6006b74f0df28c84e0554eebd6926ddfbed0
mightbegood12 <banunaresh1976@gmail.com>
2024-12-04 21:29:34 +0530

    [Refactor] AdminLink Component

--- frontend/src/components/AdminLink.jsx
+++ frontend/src/components/AdminLink.jsx
@@ -1,13 +1,14 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 export const AdminLink = () => {
   return (
-    <Link
-      className="hidden md:flex text-white font-semibold h-12 w-20 bg-gradient-to-r from-[rgb(246,102,42)] to-[#dc2626] rounded-full items-center justify-center sticky bottom-4 left-4 z-[99]"
-      to="http://localhost:5000/"
-    >
-      Admin
-    </Link>
+    <>
+      <Link
+        className="hidden md:flex text-white font-semibold h-12 w-20 bg-gradient-to-r from-[rgb(246,102,42)] to-[#dc2626] rounded-full items-center justify-center sticky bottom-4 left-4 z-[99]"
+        to=""
+      >
+        Admin
+      </Link>
+    </>
   );
 };

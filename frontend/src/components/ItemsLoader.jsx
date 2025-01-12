1d452e3e623cf19e5cf87b3230b617b27732174c
mightbegood12 <banunaresh1976@gmail.com>
2025-01-13 00:02:47 +0530

    [FIX] Items Loader

---
+++ frontend/src/components/ItemsLoader.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import DynamicButton from "./DynamicButton";
+
+export default function ItemsLoader() {
+  return (
+    <>
+      <div className="carousel mx-[10%] flex flex-col gap-1 mt-6 md:mt-8">
+        <div className="text-xl md:text-2xl font-semibold text-slate-700">
+          Categories
+        </div>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div>
+            <div>
+              <div className="itemWrapper flex flex-col drop-shadow-sm md:hover:scale-110  ease-in-out duration-300 max-w-40 flex-shrink-0 rounded-lg p-4 border-[1px] border-opacity-30 border-gray-400 m-2 items-center justify-center">
+                <div className="img object-cover animate-pulse">
+                  <div className="bg-slate-200 h-[120px] w-[120px]"></div>
+                </div>
+                <div className="info flex flex-col gap-1 items-start justify-start w-[120px]">
+                  <div className="font-semibold my-2 animate-pulse bg-slate-200 h-4 w-[60px] rounded-lg"></div>
+                  <div className="price-add-btn w-[100%] flex flex-row items-center justify-between gap-3">
+                    <div className="text-green-600 text-sm md:text-md lg:text-lg">
+                      $$$
+                    </div>
+                    <div className="dynamic-button">
+                      <DynamicButton />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="itemWrapper flex flex-col drop-shadow-sm md:hover:scale-110  ease-in-out duration-300 max-w-40 flex-shrink-0 rounded-lg p-4 border-[1px] border-opacity-30 border-gray-400 m-2 items-center justify-center">
+                <div className="img object-cover animate-pulse">
+                  <div className="bg-slate-200 h-[120px] w-[120px]"></div>
+                </div>
+                <div className="info flex flex-col gap-1 items-start justify-start w-[120px]">
+                  <div className="font-semibold my-2 animate-pulse bg-slate-200 h-4 w-[60px] rounded-lg"></div>
+                  <div className="price-add-btn w-[100%] flex flex-row items-center justify-between gap-3">
+                    <div className="text-green-600 text-sm md:text-md lg:text-lg">
+                      $$$
+                    </div>
+                    <div className="dynamic-button">
+                      <DynamicButton />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="itemWrapper flex flex-col drop-shadow-sm md:hover:scale-110  ease-in-out duration-300 max-w-40 flex-shrink-0 rounded-lg p-4 border-[1px] border-opacity-30 border-gray-400 m-2 items-center justify-center">
+                <div className="img object-cover animate-pulse">
+                  <div className="bg-slate-200 h-[120px] w-[120px]"></div>
+                </div>
+                <div className="info flex flex-col gap-1 items-start justify-start w-[120px]">
+                  <div className="font-semibold my-2 animate-pulse bg-slate-200 h-4 w-[60px] rounded-lg"></div>
+                  <div className="price-add-btn w-[100%] flex flex-row items-center justify-between gap-3">
+                    <div className="text-green-600 text-sm md:text-md lg:text-lg">
+                      $$$
+                    </div>
+                    <div className="dynamic-button">
+                      <DynamicButton />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="itemWrapper flex flex-col drop-shadow-sm md:hover:scale-110  ease-in-out duration-300 max-w-40 flex-shrink-0 rounded-lg p-4 border-[1px] border-opacity-30 border-gray-400 m-2 items-center justify-center">
+                <div className="img object-cover animate-pulse">
+                  <div className="bg-slate-200 h-[120px] w-[120px]"></div>
+                </div>
+                <div className="info flex flex-col gap-1 items-start justify-start w-[120px]">
+                  <div className="font-semibold my-2 animate-pulse bg-slate-200 h-4 w-[60px] rounded-lg"></div>
+                  <div className="price-add-btn w-[100%] flex flex-row items-center justify-between gap-3">
+                    <div className="text-green-600 text-sm md:text-md lg:text-lg">
+                      $$$
+                    </div>
+                    <div className="dynamic-button">
+                      <DynamicButton />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="itemWrapper flex flex-col drop-shadow-sm md:hover:scale-110  ease-in-out duration-300 max-w-40 flex-shrink-0 rounded-lg p-4 border-[1px] border-opacity-30 border-gray-400 m-2 items-center justify-center">
+                <div className="img object-cover animate-pulse">
+                  <div className="bg-slate-200 h-[120px] w-[120px]"></div>
+                </div>
+                <div className="info flex flex-col gap-1 items-start justify-start w-[120px]">
+                  <div className="font-semibold my-2 animate-pulse bg-slate-200 h-4 w-[60px] rounded-lg"></div>
+                  <div className="price-add-btn w-[100%] flex flex-row items-center justify-between gap-3">
+                    <div className="text-green-600 text-sm md:text-md lg:text-lg">
+                      $$$
+                    </div>
+                    <div className="dynamic-button">
+                      <DynamicButton />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,32 @@
 import ImageCarousel from "../components/ImageCarousel";
 import Categories from "../components/Categories";
 import EasyAccess from "../components/EasyAccess";
+import { Suspense } from "react";
+import ItemsLoader from "../components/ItemsLoader";
 // import { useState } from "react";
 
 export default function Home({ productData, availableCategories }) {
   return (
-    <div className="min-h-screen mb-8 md:mb-12 flex flex-col gap-2">
-      <EasyAccess categories={availableCategories} />
-      <ImageCarousel />
-      {availableCategories.map((category, index) => (
-        <Categories key={index} categoryTitle={category} items={productData} />
-      ))}
-    </div>
+    <Suspense
+      fallback={
+        <div className="min-h-screen mb-8 md:mb-12 flex flex-col gap-2">
+          <EasyAccess />
+          <ImageCarousel />
+          <ItemsLoader />
+        </div>
+      }
+    >
+      <div className="min-h-screen mb-8 md:mb-12 flex flex-col gap-2">
+        <EasyAccess categories={availableCategories} />
+        <ImageCarousel />
+        {availableCategories.map((category, index) => (
+          <Categories
+            key={index}
+            categoryTitle={category}
+            items={productData}
+          />
+        ))}
+      </div>
+    </Suspense>
   );
 }
